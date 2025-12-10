ed21c1066801d5fbf1f5dd9ca42c2b2a08a7ec07
Николай <usenkko.nikolay@gmail.com>
2025-12-10 17:47:55 +0300

    Update for task 4

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -2,7 +2,6 @@
 {
     partial class Form1
     {
-
         private System.ComponentModel.IContainer components = null;
 
         protected override void Dispose(bool disposing)
@@ -18,19 +17,70 @@
 
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            btnColor1 = new Button();
+            btnColor2 = new Button();
+            btnStartAnim = new Button();
+            btnStopAnim = new Button();
+            colorDialog1 = new ColorDialog();
+            animationTimer = new System.Windows.Forms.Timer(components);
             SuspendLayout();
-            // 
+            //
+            // btnColor1
+            //
+            btnColor1.Location = new System.Drawing.Point(520, 30);
+            btnColor1.Size = new System.Drawing.Size(160, 30);
+            btnColor1.Text = "Цвет области 1";
+            btnColor1.Click += btnColor1_Click;
+            //
+            // btnColor2
+            //
+            btnColor2.Location = new System.Drawing.Point(520, 70);
+            btnColor2.Size = new System.Drawing.Size(160, 30);
+            btnColor2.Text = "Цвет области 2";
+            btnColor2.Click += btnColor2_Click;
+            //
+            // btnStartAnim
+            //
+            btnStartAnim.Location = new System.Drawing.Point(520, 120);
+            btnStartAnim.Size = new System.Drawing.Size(160, 30);
+            btnStartAnim.Text = "Старт анимации";
+            btnStartAnim.Click += btnStartAnim_Click;
+            //
+            // btnStopAnim
+            //
+            btnStopAnim.Location = new System.Drawing.Point(520, 160);
+            btnStopAnim.Size = new System.Drawing.Size(160, 30);
+            btnStopAnim.Text = "Стоп анимации";
+            btnStopAnim.Click += this.btnStopAnim_Click;
+            //
+            // animationTimer
+            //
+            animationTimer.Interval = 300;
+            animationTimer.Tick += AnimationTimer_Tick;
+            //
             // Form1
-            // 
-            ClientSize = new Size(751, 486);
-            Name = "Form1";
-            Paint += Form1_Paint;
+            //
+            ClientSize = new System.Drawing.Size(750, 550);
+            Text = "Индиидуальное задание #1";
+            Controls.Add(btnColor1);
+            Controls.Add(btnColor2);
+            Controls.Add(btnStartAnim);
+            Controls.Add(btnStopAnim);
+            DoubleBuffered = true;
+            MouseMove += Form1_MouseMove;
             MouseDown += Form1_MouseDown;
             MouseUp += Form1_MouseUp;
             ResumeLayout(false);
-
-            #endregion
-
         }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor1;
+        private System.Windows.Forms.Button btnColor2;
+        private System.Windows.Forms.Button btnStartAnim;
+        private System.Windows.Forms.Button btnStopAnim;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Timer animationTimer;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,43 +1,183 @@
+﻿using System.Drawing.Drawing2D;
+
 namespace software_lab8
 {
     public partial class Form1 : Form
     {
+        private Color colorRight = Color.Gray;
+        private Color colorTop = Color.Gray;
+        private Color hoverColor = Color.Orange;
+
+        private bool hoverRight = false;
+        private bool hoverTop = false;
+
+        private Point axisXPos = new Point(460, 255);
+        private Point axisYPos = new Point(220, 40);
+
+        private bool dragX = false, dragY = false;
+        private bool animToggle = false;
+
         public Form1()
         {
             InitializeComponent();
-
         }
 
-        private Point location = new Point(50, 50);
-        private int R = 50;
-        Boolean DropStarted = false;
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
+        protected override void OnPaint(PaintEventArgs e)
         {
-            Graphics gr = e.Graphics;
-            gr.DrawEllipse(new Pen(Color.Red, 5), location.X - R, location.Y - R, R, R);
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            int cx = 250;
+            int cy = 250;
+            float scale = 60f;
+
+            // Радиусы
+            float R_outer = 2f * scale;
+            float R_inner = 1f * scale;
+
+            // Окружности
+            RectangleF circleMain = new RectangleF(cx - R_outer, cy - R_outer, R_outer * 2, R_outer * 2);
+            RectangleF circleInner = new RectangleF(cx - R_inner, cy - R_inner, R_inner * 2, R_inner * 2);
+            RectangleF circleShifted = new RectangleF(cx - R_outer + scale, cy - R_outer, R_outer * 2, R_outer * 2);
+
+            // Рисование окружностей
+            g.DrawEllipse(Pens.Black, circleMain);
+            g.DrawEllipse(Pens.Black, circleInner);
+            g.DrawEllipse(Pens.Black, circleShifted);
+
+            // Рисование осей
+            g.DrawLine(Pens.Black, 0, cy, 500, cy);
+            g.DrawLine(Pens.Black, cx, 0, cx, 500);
+
+            // Рисование прямой
+            g.DrawLine(new Pen(Color.Black, 2), cx - 300, cy + 300, cx + 300, cy - 300);
+
+            // Закрашенные области
+            DrawFilledRegions(g, cx, cy, scale);
+
+            // Рисование подписей
+            g.DrawString("X", Font, Brushes.Black, axisXPos);
+            g.DrawString("Y", Font, Brushes.Black, axisYPos);
+        }
+
+        private void DrawFilledRegions(Graphics g, int cx, int cy, float scale)
+        {
+            SolidBrush brRight = new SolidBrush(hoverRight ? hoverColor : colorRight);
+            SolidBrush brTop = new SolidBrush(hoverTop ? hoverColor : colorTop);
+
+            for (int px = 0; px < Width; px++)
+            {
+                for (int py = 0; py < Height; py++)
+                {
+                    float x = (px - cx) / scale;
+                    float y = (cy - py) / scale;
+
+                    bool inMain = x * x + y * y <= 4;
+                    bool inShifted = (x - 1) * (x - 1) + y * y <= 4;
+                    bool inInner = x * x + y * y <= 1;
+                    bool belowLine = y <= x;
+
+                    bool rightRegion = x >= 0 && y >= 0 && !inMain && inShifted && !inInner && belowLine;
+                    bool topRegion   = y >= 0 && x <= 0 && !inInner && inShifted && inMain;
+
+                    if (rightRegion)
+                        g.FillRectangle(brRight, px, py, 1, 1);
+
+                    if (topRegion)
+                        g.FillRectangle(brTop, px, py, 1, 1);
+                }
+            }
+
+            brRight.Dispose();
+            brTop.Dispose();
+        }
+
+        // Наведение и перетаскивание
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            int cx = 250;
+            int cy = 250;
+            float scale = 60f;
+
+            if (dragX)
+            {
+                axisXPos = e.Location;
+                Invalidate();
+                return;
+            }
+
+            if (dragY)
+            {
+                axisYPos = e.Location;
+                Invalidate();
+                return;
+            }
+
+            float x = (e.X - cx) / scale;
+            float y = (cy - e.Y) / scale;
+
+            bool inMain = x * x + y * y <= 4;
+            bool inShifted = (x - 1) * (x - 1) + y * y <= 4;
+            bool inInner = x * x + y * y <= 1;
+            bool belowLine = y <= x;
+
+            hoverRight = x >= 0 && y >= 0 && !inMain && inShifted && !inInner && belowLine;
+            hoverTop   = y >= 0 && x <= 0 && !inInner && inShifted && inMain;
+
+            Invalidate();
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            int curX = e.X, curY = e.Y;
-            Boolean InX = (curX >= location.X - R && curX <= location.X + R);
-            Boolean InY = (curY >= location.Y - R && curY <= location.Y + R);
+            if (new Rectangle(axisXPos, new Size(20, 20)).Contains(e.Location))
+                dragX = true;
 
-            if (InX && InY)
-            {
-                DropStarted = true;
-            }
-
+            if (new Rectangle(axisYPos, new Size(20, 20)).Contains(e.Location))
+                dragY = true;
         }
+
         private void Form1_MouseUp(object sender, MouseEventArgs e)
         {
-            if (DropStarted)
+            dragX = false;
+            dragY = false;
+        }
+
+        // Выбор цвета
+        private void btnColor1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
             {
-                location.X = e.X + R / 2;
-                location.Y = e.Y + R / 2;
+                colorRight = colorDialog1.Color;
                 Invalidate();
             }
+        }
+
+        private void btnColor2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                colorTop = colorDialog1.Color;
+                Invalidate();
+            }
+        }
+
+        // Анимация
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            hoverColor = animToggle ? Color.Red : Color.Blue;
+            animToggle = !animToggle;
+            Invalidate();
+        }
+
+        private void btnStartAnim_Click(object sender, EventArgs e)
+        {
+            animationTimer.Start();
+        }
+
+        private void btnStopAnim_Click(object sender, EventArgs e)
+        {
+            animationTimer.Stop();
         }
     }
 }
